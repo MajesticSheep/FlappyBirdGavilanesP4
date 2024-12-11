dc5acc831d3248b630db385f83f4324976b33809
Giannamia Gavilanes <719613@student.dusd.net>
2024-12-11 11:45:10 -0800

    Finished the scripts
    I created scripts to make the game functional, and inserted my own sprites for the bird and columns to stop the bird.

--- FlappyBirdP4Gavilanes/Assets/Scripts/GameController.cs
+++ FlappyBirdP4Gavilanes/Assets/Scripts/GameController.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
 
     public static GameController instance;
     public GameObject gameOverText;
+    public TextMeshProUGUI scoreText;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
+
+    private int score = 0;
     
     void Awake ()
     {
@@ -36,6 +41,16 @@ public class GameController : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+    }
+
+    public void BirdScore()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
     }
 
     public void BirdDied()
